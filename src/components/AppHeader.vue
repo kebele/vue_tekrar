65ffd2b8a6ef80e936293dce953668302132de31
kebele <51454858+kebele@users.noreply.github.com>
2020-10-29 03:54:34 +0300

    ekleme

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,32 @@
 <template>
-  <nav
-    class="bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2"
-  >
-    <a href="#">DC Heroes</a>
+  <nav class="bg-gradient-to-r from-blue-800 to-blue-600 text-white px-4 py-2">
+    <!-- <a href="#">DC Heroes</a> -->
+    <!-- <router-link class="mx-2" to="/dc-heroes">dc heroes</router-link>
+    <router-link class="mx-2" to="/calendar">calendar</router-link>
+    <router-link class="mx-2" to="/markdown">markdown</router-link> -->
+    <!-- v-for ile yapalım linkleri -->
+    <router-link
+      class="mx-2"
+      v-for="item in list"
+      :key="item.to"
+      :to="item.to"
+      >{{ item.title }}</router-link
+    >
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      list: [
+        { title: "dc heroes", to: "/dc-heroes" },
+        { title: "calendar", to: "/calendar" },
+        { title: "markdown", to: "/markdown" },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped></style>
---
+++ src/router.js
@@ -0,0 +1,26 @@
+
+
+// import VueRouter from 'vue-router'
+import { createRouter, createWebHistory} from 'vue-router'
+import DcHeroes from './pages/DcHeroes.vue'
+import Calendar from './pages/Calendar.vue'
+import Home from './pages/Home.vue'
+import Markdown from './pages/Markdown.vue'
+
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/dc-heroes', component: DcHeroes },
+    { path: '/calendar', component: Calendar },
+    { path: '/markdown', component: Markdown },
+]
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+// adres satırındaki # işaretinin kalkması için createwebhashHistory değil createWebHistory kullanmalıyız, 
+export default router;
