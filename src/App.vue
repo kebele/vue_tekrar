ce449b653c61629d0cc8c3e78fa4d3174abfeed2
kebele <51454858+kebele@users.noreply.github.com>
2020-10-29 03:53:15 +0300

    ekleme

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,21 @@
   <div>
     <AppHeader />
     <div class="w-full flex">
-      <Calendar />
+      <router-view></router-view>
+      <!-- <Calendar /> -->
     </div>
   </div>
 </template>
 
 <script>
 import AppHeader from "./components/AppHeader";
-import Calendar from "./components/Calendar"
+// import Calendar from "./components/Calendar"
 
 
 export default {
   components: {
     AppHeader,
-    Calendar
+    // Calendar
   },
 };
 </script>
